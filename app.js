f7204145c645cd3c80b089ca05a223df4cbf0c8f
Joe Karlsson <joe.carlson@callmejoe.net>
2019-09-11 18:47:41 -0500

    switch and weights kinda working

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const { RaspiIO } = require('raspi-io');
 const five = require('johnny-five');
+
 const board = new five.Board({
     io: new RaspiIO()
 });
 
 board.on('ready', () => {
-    // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
-    const led = new five.Led('P1-13');
-    led.strobe(500);
+    const sensor = new five.Sensor.Digital({
+        pin: "GPIO5",
+        freq: 100,
+        // threshold: 5,
+    });
+
+    // sensor.on("data", (output) => {
+    //     console.log("sensor", sensor.raw);
+    // });
+
+    sensor.on("change", () => {
+        console.log("change", sensor.raw);
+        // this.value will reflect a scaling from 0-1023 to 0-180
+        // console.log(sensor.scaleTo([0, 180])); // integer
+    });
+
+
+    const spdt = new five.Switch("GPIO16");
+
+    spdt.on("open", () => {
+        console.log('open');
+    });
+
+    spdt.on("close", () => {
+        console.log('close');
+    });
 });
