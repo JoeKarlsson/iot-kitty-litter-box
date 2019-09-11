3e8f75fe01328153b664fc9cd17849025b06f440
Joe Karlsson <joekarlsson1@gmail.com>
2019-09-10 20:21:02 -0500

    blinking works!

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const { RaspiIO } = require('raspi-io');
+const five = require('johnny-five');
+const board = new five.Board({
+    io: new RaspiIO()
+});
+
+board.on('ready', () => {
+    // Create an Led on pin 7 on header P1 (GPIO4) and strobe it on/off
+    const led = new five.Led('P1-13');
+    led.strobe(500);
+});
